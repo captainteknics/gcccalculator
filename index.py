bf15f8884fe18d5dd5f362ef9b65d5cfe43d1393
Gilbert Nakiboli Waliuba <captainteknics@gmail.com>
2020-06-13 04:07:32 +0300

    Create index.py
    gcccalculator

---
+++ index.py
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Discover Any Names and magic Meanings || Use the Get Critical Calculator</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+<meta  name="author" content="Nakiboli Gilbert -0715560734" >
+<meta  name="description" content="Discover the Magic in Names by the Get Critical Calculator, Critical Thinking Coach in Nairobi, IQ analysis tools for 2020 and beyond, Names and meanings Calculator" >
+  <link rel="shortcut icon" href="gcc.jpg">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P7MNG2H');</script>
+<!-- End Google Tag Manager -->
+  <style>
+
+    body{
+      font-size: 20px;
+    }
+
+    input{
+      color:blue;
+    }
+  .bg-1 { 
+    background-color: #1abc9c;
+    color: #ffffff;
+  }
+  .bg-2 { 
+    background-color: #474e5d;
+    color: #ffffff;
+  }
+  .bg-3 { 
+    background-color: #ffffff;
+    color: #555555;
+  }
+  .bg-4 { 
+    background-color: #2f2f2f;
+    color: #fff;
+  }
+  .container-fluid {
+    padding-top: 70px;
+    padding-bottom: 70px;
+  }
+  .navbar {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border: 0;
+    border-radius: 0;
+    margin-bottom: 0;
+    font-size: 12px;
+    letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+    color: #1abc9c !important;
+  }
+  </style>
+</head>
+<body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7MNG2H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">GetCriticalCalculator</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#about">About GCC</a></li>
+        <li><a href="tilldonate.jpg"target="_blank" title="3EwkDY9a1kyaTY1fkSqMRKhSwcAr693Za1">Buy Me Coffee</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid bg-1 text-center">
+  <h1>Get Critical Calculator</h1>
+  <img src="gcc.jpg" class="img-responsive img-circle" style="display:inline" alt="critical thinking coach in Nairobi" width="350" height="350">
+  <h3>By Nakibolis </h3>
+
+</div>
+
+<div class="container-fluid bg-2 text-center" id="about">
+  <h3>What Am I?</h3>
+  <p style="text-align: center;" >Critical and analytical thinking means getting the most neglected details like the length of your Name. Calculate the length of your names and discover what they mean. I just did mine and am  the <b>Boeing "787"</b>- Modest and Powerful Jet</p>
+
+  <h4 id="calculate">Get Started</h4>
+
+  <form action="index.php" method="POST">
+
+    <input type="text" name="firstName" placeholder="First Name" minlength="3" maxlength="14" required><h1></h1>
+
+
+    <input type="text"  name="middleName" placeholder="Middle Name" minlength="3" maxlength="14" required><h1></h1>
+
+  
+    <input type="text" name="lastName" placeholder="Last Name" minlength="3" maxlength="14" required><h1></h1>
+
+    
+
+  
+<span><input type="submit" name="submit" value="Confirm"></span>
+<h1></h1>
+  <br>
+
+</form>
+
+<?php
+
+
+
+$firstName= $_POST['firstName'];
+$middleName= $_POST['middleName'];
+$lastName= $_POST['lastName'];
+
+
+
+$lenthF=strlen($firstName);
+$lenthM=strlen($middleName);
+$lenthL=strlen($lastName);
+$nameCount= ($lenthF+$lenthM+$lenthL);
+$nameCombined="$lenthF$lenthM$lenthL";
+
+
+
+ echo "<p style='color:yellow'>Hello  <strong>$firstName  $middleName  $lastName </strong> Your Name length is:   <strong>$nameCount</strong> <h1></h1> 
+
+Your Name Magic Combination is: echo <strong>$nameCombined</strong> <h1></h1>
+
+ Click <a id='blink' style='color:yellow' href='https://www.google.com/search?q=$nameCombined&oq=$nameCombined&aqs=chrome..69i57j0l7.3047j0j8&sourceid=chrome&ie=UTF-8'   target='_blank'>Here</a> to Confirm what Your Magic Name Number means <p>";?>
+     
+</div>
+
+<div class="container-fluid bg-3 text-center">    
+  <h3>Your Name in Numbers?</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <?php echo  "<p>Your First Name <b>'$firstName'</b> is <strong>$lenthF</strong>  Characters Long</p>" ; ?>
+    </div>
+    <div class="col-sm-4"> 
+    <?php echo "<p> Your Middle Name <b>'$middleName'</b> is <strong>$lenthM</strong> Characters Long</p>"; ?>
+    </div>
+    <div class="col-sm-4"> 
+     <?php echo "<p>Your Last Name <b>'$lastName'</br> is   <strong>$lenthL</strong>  Characters Long</p>"; ?>
+    </div>
+<br>
+<hr>
+    <h3> <a href="#calculate">Enyoy the magic, Check Your Friends' Name</a></h3>
+
+
+  </div>
+  <h4> Don't enjoy alone: Share with a friend on <a href="https://api.whatsapp.com/send?&text=Join%20Me%20on%20gcccalculator.co.ke%20for%20magic%20bulk%20names%20analysis" target="_blank">WhatsApp</h4>
+</div>
+
+<!--blinkingText-->
+<script>
+        var ops,blink=document.getElementById('blink');
+        ops=1
+        setInterval(function (){
+            ops = (ops < 1) ? 1:0;
+            blink.style.opacity=ops;
+        },700);
+    </script>
+
+
+<footer class="container-fluid bg-4 text-center">
+  <p> &copy <a href="https://twitter.com/captainteknics">CaptainTeknics Inc</a> || Powered By <a href="https://about.me/waliuba" target="_blank">Gilbert (787) </a> Design Thinking Coach in Nairobi</p> 
+</footer>
+
+</body>
+</html>
